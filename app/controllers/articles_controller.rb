a156f246e91e35e940ef0e856932b2f2a38b898b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-02-02 16:30:17 +0900

    CRUD操作の実装完了

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,52 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[ show edit update destroy]
 
   def index
     @articles = Article.all
   end
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    if @article.save
+      redirect_to articles_path
+    else 
+      render :new, status:  :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @article.update(article_params)
+      redirect_to articles_path, notice: "記事の更新が成功しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to articles_path, notice: "記事の削除に成功しました"
+    else
+      redirect_to articles_path, notice: "記事の削除に失敗しました"
+    end
+  end
+
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
+
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
 end

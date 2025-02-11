2ad57b87fa64b006d71f82e1ec2d538ac1aace7b
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-02-11 11:39:01 +0900

    gemin基本設定完了

--- app/controllers/gemini_controller.rb
+++ app/controllers/gemini_controller.rb
@@ -1,6 +1,6 @@
 class GeminiController < ApplicationController
   def show
-    client = Gemini.new
-    @response = client.generate_content('始めてAPI通信をしています。成功しましたか？')
+    client = Gemini.new('今日の沖縄の天候を簡単に教えてください')
+    @response = client.generate_content
   end
 end
--- app/services/gemini.rb
+++ app/services/gemini.rb
@@ -1,29 +1,82 @@
 class Gemini
   include HTTParty 
-  BASE_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent'
+  base_uri 'https://generativelanguage.googleapis.com/v1beta/models'
+  
 
-
-  def initialize
+  def initialize(prompt)
     @api_key = Rails.application.credentials[:gemini][:api_key]
+    @prompt = prompt
   end
 
-  def generate_content(prompt)
-    response = self.class.post(
-      "#{BASE_URL}?key=#{@api_key}",
-      headers: { "Content-Type" => "application/json" },
-      body: {
-        contents: [
-          {
-            parts: [{ text: prompt }]
-          }
-        ] 
-      }.to_json
-    )
+
+  def generate_content
+    options = {
+      query: {key: @api_key},
+      headers: { 'Content-Type' => 'application/json'},
+      body: { 
+        contents: [{
+          parts: [{ text: @prompt }]
+      }]}.to_json
+    }
+
+    response = self.class.post('/gemini-2.0-flash:generateContent',options)
 
     if response.code == 200
-      JSON.parse(response.body.to_s)
+      formatted_response(response.parsed_response.deep_symbolize_keys)
+      #JSON.parse(response.body, symbolize_names: true)
     else
-      { error: 'API request failed', status: response.status }
+      { error: 'API request failed', status: response.code }
     end
   end
+
+  private
+
+  def formatted_response(response)
+    response[:candidates][0][:content][:parts][0][:text]
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#   BASE_URL = 'https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent'
+
+
+#   def initialize
+#     @api_key = Rails.application.credentials[:gemini][:api_key]
+#   end
+
+#   def generate_content(prompt)
+#     response = self.class.post(
+#       "#{BASE_URL}?key=#{@api_key}",
+#       headers: { "Content-Type" => "application/json" },
+#       body: {
+#         contents: [
+#           {
+#             parts: [{ text: prompt }]
+#           }
+#         ] 
+#       }.to_json
+#     )
+
+#     if response.code == 200
+#       JSON.parse(response.body.to_s, symbolize_names: true)
+#     else
+#       { error: 'API request failed', status: response.code }
+#     end
+#   end
+# end

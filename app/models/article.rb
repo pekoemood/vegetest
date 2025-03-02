a954788d93413140d909f8bb665e1c36bec26d83
Ryotarou Shiozawa <pekoemood@gmail.com>
2025-03-02 23:45:38 +0900

    like機能の実装完了

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
   validates :title, presence: true
